cf54e7a6d83c8eaed7b9fb0b0658414311179d9e
after1990s <after1990s@gmail.com>
2015-11-30 14:03:31 +0800

    add release makefile

---
+++ Release/src/subdir.mk
@@ -0,0 +1,48 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/autocritical.cpp \
+../src/socks5protocol.cpp \
+../src/socks5protocol_test.cpp \
+../src/udtconfig.cpp \
+../src/udtforwardclient.cpp \
+../src/udtforwardclient_test.cpp \
+../src/udtsocks.cpp \
+../src/udtsocksserver.cpp \
+../src/udtwrapper.cpp 
+
+OBJS += \
+./src/autocritical.o \
+./src/socks5protocol.o \
+./src/socks5protocol_test.o \
+./src/udtconfig.o \
+./src/udtforwardclient.o \
+./src/udtforwardclient_test.o \
+./src/udtsocks.o \
+./src/udtsocksserver.o \
+./src/udtwrapper.o 
+
+CPP_DEPS += \
+./src/autocritical.d \
+./src/socks5protocol.d \
+./src/socks5protocol_test.d \
+./src/udtconfig.d \
+./src/udtforwardclient.d \
+./src/udtforwardclient_test.d \
+./src/udtsocks.d \
+./src/udtsocksserver.d \
+./src/udtwrapper.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -D__GXX_EXPERIMENTAL_CXX0X__ -Iudt -Ipthread -O3 -Wall -c -fmessage-length=0 -std=c++11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
