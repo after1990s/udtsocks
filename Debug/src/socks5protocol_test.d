fe0f32ba609dea8a18ac0df05805f627c915204b
after1990s <after1990s@gmail.com>
2015-11-30 13:52:11 +0800

    beta 0.2,solve memory leak

---
+++ Debug/src/socks5protocol_test.d
@@ -0,0 +1,11 @@
+src/socks5protocol_test.d: ../src/socks5protocol_test.cpp \
+ ../src/socks5protocol.h ../src/autocritical.h ../src/socks5.h \
+ ../src/udtconfig.h
+
+../src/socks5protocol.h:
+
+../src/autocritical.h:
+
+../src/socks5.h:
+
+../src/udtconfig.h:
---
+++ Release/src/udtsocksserver.d
@@ -0,0 +1,15 @@
+src/udtsocksserver.d: ../src/udtsocksserver.cpp ../src/udtsocksserver.h \
+ ../src/udtwrapper.h ../src/socks5.h ../src/socks5protocol.h \
+ ../src/autocritical.h ../src/udtconfig.h
+
+../src/udtsocksserver.h:
+
+../src/udtwrapper.h:
+
+../src/socks5.h:
+
+../src/socks5protocol.h:
+
+../src/autocritical.h:
+
+../src/udtconfig.h:
